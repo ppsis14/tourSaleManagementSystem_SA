4a00a7053daaae470523c80ad55f39c22bd9b9e8
thikamporn <thikamporn.s@ku.th>
2018-12-24 13:08:16 +0700

    edit validate ver.3

--- src/main/java/tourSaleManagementController/ReservePageController.java
+++ src/main/java/tourSaleManagementController/ReservePageController.java
@@ -198,7 +198,8 @@ public class ReservePageController implements Initializable {
     void handleAddCustomerBtn(ActionEvent event) {
         System.out.println("countErr : " + countErr);
         countErr = 0;
-        /*if (checkFillOutInformation()) {
+        checkFillOutInformation();
+            /*
             String tour_id = manageableDatabase.getTourID(tourIDComboBox.getSelectionModel().getSelectedItem());
             int availableSeat = manageableDatabase.getAvailableByTourID(manageableDatabase.getTourID(tourIDComboBox.getSelectionModel().getSelectedItem()));
 
@@ -461,7 +462,6 @@ public class ReservePageController implements Initializable {
 
     void setUpValueReservationPage(){
         //setup value of reservation page
-
         customerNo.setText("1");     // setup count amount customer
         clearText();
         customerList.clear();
@@ -499,279 +499,96 @@ public class ReservePageController implements Initializable {
         return amountReservationSeatLabel;
     }
 
-    public Boolean checkFillOutInformation() {
+    public void checkFillOutInformation() {
         if (validateFieldsIsEmpty()){
-            return false;
+            Alert alertFillOutInformationError = new Alert(Alert.AlertType.ERROR);
+            alertFillOutInformationError.setTitle("Error Dialog");
+            alertFillOutInformationError.setHeaderText("Saving customer information is error!");
+            alertFillOutInformationError.setContentText("Please completely fill out information follow (*)");
+            Optional<ButtonType> checkFillOutInformationAction = alertFillOutInformationError.showAndWait();
+        }
+        else if (!allFieldIsAccuracy()){
+            Alert alertFillOutPatternError = new Alert(Alert.AlertType.ERROR);
+            alertFillOutPatternError.setTitle("Error Dialog");
+            alertFillOutPatternError.setHeaderText("Saving customer information is error!");
+            alertFillOutPatternError.setContentText("Some fields are not yet accurate, please fill form again");
+            Optional<ButtonType> checkPatternErrorAction = alertFillOutPatternError.showAndWait();
+            if (checkPatternErrorAction.get() == ButtonType.OK){ countErr = 0; }
         }
         else{
-            //System.out.println("countErr: " + countErr);;
-            return true;
+            String tour_id = manageableDatabase.getTourID(tourIDComboBox.getSelectionModel().getSelectedItem());
+            int availableSeat = manageableDatabase.getAvailableByTourID(manageableDatabase.getTourID(tourIDComboBox.getSelectionModel().getSelectedItem()));
+
+            if (availableSeat - Integer.valueOf(customerNo.getText()) >= 0) {
+                //---------------------------------------------------------
+                // case show "Do you want to save this customer information?" -> ok = continue/ cancel = add another customer
+                Alert alertSaveCustomerInformation = new Alert(Alert.AlertType.CONFIRMATION);
+                alertSaveCustomerInformation.setTitle("Confirmation Dialog");
+                alertSaveCustomerInformation.setHeaderText(null);
+                alertSaveCustomerInformation.setContentText("Do you want to save this customer information?");
+                Optional<ButtonType> saveCustomerInfoAction = alertSaveCustomerInformation.showAndWait();
+
+                if (saveCustomerInfoAction.get() == ButtonType.OK) {
+
+                    setCustomerFromGUI();
+                    //add new customer to list for check
+                    if (newCustomer.isSelected()) {
+                        newCustomerID_List.add(customer.getCustomerID());
+                    }
+                    //add customer id to list
+                    customerList.add(customer);
+                    setReservationCustomerFromGUI();
+                    reserveCustomer_List.add(reservationCustomer);
+
+                    Alert alertAddMoreCustomer = new Alert(Alert.AlertType.CONFIRMATION);
+                    alertAddMoreCustomer.setTitle("Confirmation Dialog");
+                    alertAddMoreCustomer.setHeaderText(null);
+                    alertAddMoreCustomer.setContentText("Do you want to add another customer?");
+                    Optional<ButtonType> addMoreCustomerAction = alertAddMoreCustomer.showAndWait();
+                    if (addMoreCustomerAction.get() == ButtonType.OK) {
+
+                        // add count amount customer
+                        customerNo.setText(String.valueOf(Integer.valueOf(customerNo.getText()) + 1));
+                        //set reserv seat
+                        String[] reservText = reservedSeats.getText().split(" / ");
+                        reservedSeats.setText(String.valueOf( Integer.valueOf(reservText[0]) + 1 ) + " / " + reservText[1]);
+
+                    }
+
+                    else if (addMoreCustomerAction.get() == ButtonType.CANCEL) {
+                        //disable btn -----> after press submit
+                        addCustomerBtn.setDisable(true);
+                        submitBtn.setDisable(false);
+
+                    }
+
+                    //After choose btn OK/CANCEL
+                    clearText();
+                    searchByCustomerName.clear();
+                    searchByCustomerName.setDisable(true);
+                    searchCustomerBtn.setDisable(true);
+                    customer = new Customer();
+                    reservationCustomer = new Reservation();
+
+                } else if (saveCustomerInfoAction.get() == ButtonType.CANCEL) {
+                    // back to edit customer information
+                }
+            } else {
+                //pop up warning
+                Alert alertShowInformationIsUpdate = new Alert(Alert.AlertType.INFORMATION);
+                alertShowInformationIsUpdate.setTitle("Information Dialog");
+                alertShowInformationIsUpdate.setHeaderText(null);
+                alertShowInformationIsUpdate.setContentText("Available seat are full.");
+                Optional<ButtonType> action = alertShowInformationIsUpdate.showAndWait();
+                setUpValueReservationPage();
+                if (action.get() == ButtonType.OK) {
+                    setUpValueReservationPage();
+                }
+            }
+
         }
     }
 
-    public void setValidateOnEventHandler(){
-        /*occupation.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-
-        homeTelNum.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-        faxNum.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-        underlyingDisease.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-        foodAllergy.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-        moreDetail.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                occupation.setStyle("-fx-border-color: #2C3E50");
-            }
-        });
-*/
-        firstNameTH.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateFirstNameTH()){
-                    firstNameTH.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    firstNameTH.setStyle("-fx-border-color: #922B21");
-                    System.out.println("cnt th");
-                    countErr++;
-                }
-
-            }
-        });
-
-        lastNameTH.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateLastNameTH()){
-                    lastNameTH.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    lastNameTH.setStyle("-fx-border-color: #922B21");
-                    System.out.println("cnt lnameth");
-                    countErr++;
-                }
-            }
-        });
-
-        firstNameEN.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateFirstNameEN()){
-                    firstNameEN.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    firstNameEN.setStyle("-fx-border-color: #922B21");
-                    System.out.println("cnt en");
-                    countErr++;
-                }
-            }
-        });
-
-        lastNameEN.setOnKeyReleased(new EventHandler<KeyEvent>(){
-            boolean status = false;
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateLastNameEN()){
-                    lastNameEN.setStyle("-fx-border-color: #27AE60");
-                    System.out.println("true");
-                    //status = true;
-                }else{
-                    lastNameEN.setStyle("-fx-border-color: #922B21");
-                    System.out.println("false");
-
-                }
-                /*if (status == false) {
-                    System.out.println("false");
-                    countErr++;
-                }*/
-            }
-        });
-
-        passportNo.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validatePassportNo()){
-                    passportNo.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    passportNo.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        expPassportDate.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                expPassportDate.setStyle("-fx-border-color: #2C3E50");
-                countErr++;
-            }
-        });
-
-        address.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateAddress()){
-                    address.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    address.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        phoneNum.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validatePhoneNum()){
-                    phoneNum.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    phoneNum.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        homeTelNum.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateHomeTelNum()){
-                    homeTelNum.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    homeTelNum.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        faxNum.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateFaxNum()){
-                    faxNum.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    faxNum.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        underlyingDisease.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateUnderDisease()){
-                    underlyingDisease.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    underlyingDisease.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        foodAllergy.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateFoodAllergy()){
-                    foodAllergy.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    foodAllergy.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        moreDetail.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateMoreDetail()){
-                    moreDetail.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    moreDetail.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        occupation.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if (validateOccupation()){
-                    occupation.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    occupation.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        email.setOnKeyReleased(new EventHandler<KeyEvent>(){
-
-            @Override
-            public void handle(KeyEvent event) {
-                if(validateEmail()){
-                    email.setStyle("-fx-border-color: #27AE60");
-                }else{
-                    email.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-            }
-        });
-
-        dateOfBirth.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (validateDateOfBirth()) dateOfBirth.setStyle("-fx-border-color: #27AE60");
-                else {
-                    dateOfBirth.setStyle("-fx-border-color: #922B21");
-                    countErr++;
-                }
-                if (!expPassportDate.getEditor().getText().equals("")) {
-                    compareDOBAndExp();
-                }
-            }
-        });
-
-        expPassportDate.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (!dateOfBirth.getEditor().getText().equals("")) {
-                    compareDOBAndExp();
-                }
-            }
-        });
-
-    }
     private void compareDOBAndExp(){
         if (validateDateExpPassport()) expPassportDate.setStyle("-fx-border-color: #27AE60");
         else { expPassportDate.setStyle("-fx-border-color: #922B21");countErr++;}
@@ -927,7 +744,118 @@ public class ReservePageController implements Initializable {
         }
     }
 
-    private void allFeildIsAccurency(){
-        return;
+    private boolean allFieldIsAccuracy(){
+        if(validateFirstNameTH()){
+            firstNameTH.setStyle("-fx-border-color: #27AE60");
+        }else{
+            firstNameTH.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateLastNameTH()){
+            lastNameTH.setStyle("-fx-border-color: #27AE60");
+        }else{
+            lastNameTH.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateFirstNameEN()){
+            firstNameEN.setStyle("-fx-border-color: #27AE60");
+        }else{
+            firstNameEN.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateLastNameEN()){
+            lastNameEN.setStyle("-fx-border-color: #27AE60");
+        }else{
+            lastNameEN.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validatePassportNo()){
+            passportNo.setStyle("-fx-border-color: #27AE60");
+        }else{
+            passportNo.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateAddress()){
+            address.setStyle("-fx-border-color: #27AE60");
+        }else{
+            address.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validatePhoneNum()){
+            phoneNum.setStyle("-fx-border-color: #27AE60");
+        }else{
+            phoneNum.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateHomeTelNum()){
+            homeTelNum.setStyle("-fx-border-color: #27AE60");
+        }else{
+            homeTelNum.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateFaxNum()){
+            faxNum.setStyle("-fx-border-color: #27AE60");
+        }else{
+            faxNum.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateUnderDisease()){
+            underlyingDisease.setStyle("-fx-border-color: #27AE60");
+        }else{
+            underlyingDisease.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateFoodAllergy()){
+            foodAllergy.setStyle("-fx-border-color: #27AE60");
+        }else{
+            foodAllergy.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateMoreDetail()){
+            moreDetail.setStyle("-fx-border-color: #27AE60");
+        }else{
+            moreDetail.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if (validateOccupation()){
+            occupation.setStyle("-fx-border-color: #27AE60");
+        }else{
+            occupation.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if(validateEmail()){
+            email.setStyle("-fx-border-color: #27AE60");
+        }else{
+            email.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+
+        if (validateDateOfBirth()) dateOfBirth.setStyle("-fx-border-color: #27AE60");
+        else {
+            dateOfBirth.setStyle("-fx-border-color: #922B21");
+            countErr++;
+        }
+        if (!expPassportDate.getEditor().getText().equals("")) {
+            compareDOBAndExp();
+        }
+        if (!dateOfBirth.getEditor().getText().equals("")) {
+            compareDOBAndExp();
+        }
+
+        if (countErr == 0) return true;
+        return false;
     }
 }
